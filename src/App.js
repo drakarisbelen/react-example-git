433c2136a4cec655ba5dc683eb77b1ba97c7d869
karina_escobar <karina.escobar@greenmonks.net>
2022-04-17 22:39:38 -0300

    vs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from "./logo.svg";
+import logo from "./assets/lisa.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo1" alt="logo" />
         <h1>React application - this is my app</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
